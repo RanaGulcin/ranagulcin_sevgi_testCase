044d3ca70b06cb8769edd7f288458da6be81ba23
rana gulcin <ranagulcin@gmail.com>
2025-11-20 17:04:28 +0300

    utilities oluşturuldu

--- src/test/java/utilties/ConfigReader.java
+++ src/test/java/utilties/ConfigReader.java
@@ -1,4 +1,30 @@
 package utilties;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+
+    static Properties properties;
+
+    static {
+
+        String fileLine = "src/configuration.properties";
+        try {
+            FileInputStream file = new FileInputStream(fileLine);
+            properties = new Properties();
+            properties.load(file);
+        }catch (IOException e){
+            System.out.println("configuration.properties can not read.");
+        }
+
+    }
+
+    public static String getProperty(String key){
+
+        return properties.getProperty(key);
+
+    }
+
 }
