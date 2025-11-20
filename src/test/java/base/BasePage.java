f905b262688dcebdafc14a2df7f8bce670982a60
rana gulcin <ranagulcin@gmail.com>
2025-11-20 17:18:02 +0300

    baseTest - basePage classları temel halleri

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,4 +1,15 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import utilties.Driver;
+
 public class BasePage {
+
+    protected WebDriver driver;
+
+    public BasePage(){
+        this.driver = Driver.getDriver();
+        PageFactory.initElements(driver,this);
+    }
 }
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,18 @@
 package base;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import utilties.Driver;
+
 public class BaseTest {
+
+    @BeforeClass
+    public void setup(){
+        Driver.getDriver();
+    }
+
+    @AfterClass
+    public void quit(){
+        Driver.quitDriver();
+    }
 }
